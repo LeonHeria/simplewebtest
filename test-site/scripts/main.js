9862a6e56427d3f097a440db9b85d1d359a2787a
Heria <hleocoding@gmail.com>
2023-07-04 17:04:57 +0800

    070401

---
+++ test-site/scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/kongfupanda.jpg') {
+      myImage.setAttribute('src', 'images/kongfupanda2.jpeg');
+    } else {
+      myImage.setAttribute('src', 'images/kongfupanda.jpg');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName) {
+        setUserName();
+      } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+      }
+  }
+  
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+  
+  myButton.onclick = function() {
+    setUserName();
+ }
